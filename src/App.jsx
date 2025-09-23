77da3559871b5afef3ca33f352a357496df63ed2
Dasari Pavan Kumar <pavankumardasari8639@gmail.com>
2025-09-23 12:20:17 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div>App</div>
+    <Footer/>
+    </>
+    
+  )
+}
+
+export default App
